44a8794911040a5f55a2507966517021a7a886ee
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-11-16 13:40:09 -0500

    refactor: fix auto-fixable  problems with prettier

--- songbird/webpack.config.js
+++ songbird/webpack.config.js
@@ -4,24 +4,24 @@
 // npm i eslint -D
 // npm i prettier -D
 // npm i eslint-config-prettier -D
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 // npm i cross-env --D
-const isDev = process.env.NODE_ENV === 'development';
-const isProd = !isDev;
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
 
 // const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
-const filename = (ext) => `[name].${ext}`;
-const pages = ['index', 'quiz', 'results'];
+const filename = (ext) => `[name].${ext}`
+const pages = ['index', 'quiz', 'results']
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
-        index: "./pages/index/index.js",
-        quiz: "./pages/quiz/quiz.js",
-        results: "./pages/results/results.js",
+        index: './pages/index/index.js',
+        quiz: './pages/quiz/quiz.js',
+        results: './pages/results/results.js',
     },
     mode: 'development',
     devtool: isProd ? false : 'inline-source-map',
@@ -31,53 +31,61 @@ module.exports = {
         compress: true,
         hot: true,
         port: 3000,
-        watchFiles: ['src/pages/index/index.html', 'src/pages/quiz/quiz.html', 'src/pages/results/results.html'],
+        watchFiles: [
+            'src/pages/index/index.html',
+            'src/pages/quiz/quiz.html',
+            'src/pages/results/results.html',
+        ],
     },
-    output: {    
+    output: {
         filename: `./${filename('js')}`,
         path: path.resolve(__dirname, 'dist'),
         publicPath: '',
         // Все assets будут складываться в dist/assets
-        assetModuleFilename: 'assets/[contenthash][ext][query]', 
+        assetModuleFilename: 'assets/[contenthash][ext][query]',
     },
     optimization: {
         splitChunks: {
-          chunks: "all",
+            chunks: 'all',
         },
     },
     plugins: [].concat(
-        pages.map((page) =>
-            new HtmlWebpackPlugin({
-                inject: true,
-                template: path.resolve(__dirname, `src/pages/${page}/${page}.html`),
-                filename: `${page}.html`,
-                favicon: 'assets/favicon/favicon.svg',
-                chunks: [page],
-                minify: {
-                  collapseWhitespace: isProd,
-                },
-            })
+        pages.map(
+            (page) =>
+                new HtmlWebpackPlugin({
+                    inject: true,
+                    template: path.resolve(
+                        __dirname,
+                        `src/pages/${page}/${page}.html`
+                    ),
+                    filename: `${page}.html`,
+                    favicon: 'assets/favicon/favicon.svg',
+                    chunks: [page],
+                    minify: {
+                        collapseWhitespace: isProd,
+                    },
+                })
         ),
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin()
     ),
     module: {
         rules: [
             // HTML-loader
             {
                 test: /\.html$/,
-                use: 'html-loader'
+                use: 'html-loader',
             },
             // npm i html-loader -D
-            /** Babel **/
+            /** Babel */
             {
                 test: /\.m?js$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                        presets: ['@babel/preset-env'],
+                    },
+                },
                 // npm i babel-loader @babel/core @babel/preset-env -D
             },
             /** CSS */
@@ -91,11 +99,11 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // Creates `style` nodes from JS strings
-                    "style-loader",
+                    'style-loader',
                     // Translates CSS into CommonJS
-                    "css-loader",
+                    'css-loader',
                     // Compiles Sass to CSS
-                    "sass-loader",
+                    'sass-loader',
                 ],
                 // npm i style-loader css-loader sass sass-loader -D
             },
@@ -121,4 +129,4 @@ module.exports = {
             },
         ],
     },
-};
+}
