4e7762aae6da8deac0a2f1412bcff1c798204e8a
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-11-21 12:34:09 -0500

    feat: add sound for game end

--- songbird/src/pages/results/results.js
+++ songbird/src/pages/results/results.js
@@ -1,11 +1,14 @@
 import '../../scss/style.scss';
 
 import renderScore from '../../modules/renderScore';
+import gameWin from '../../assets/audio/game-win.mp3';
 
 // eslint-disable-next-line no-console
 console.log(
     'Самооценка - 210 баллов.\nВыполнены все пункты задания, кроме Extra scope.\nСпасибо за проверку и удачи :)'
 );
+
+const soundGameWin = new Audio(gameWin);
 
 const resultEl = document.querySelector('.results .text span');
 const btnNewGameEl = document.querySelector('.results .btn');
@@ -14,7 +17,7 @@ const textEl = document.querySelector('.results .text');
 const score = localStorage.getItem('scoreOKh');
 
 renderScore(score);
-
+soundGameWin.play();
 resultEl.textContent = score;
 
 if (score === 30) {
